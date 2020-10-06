952f9e945135cce9f3ed153cbd5fb26bcc7c7219
Andrejus <anba8005@gmail.com>
2020-10-06 19:22:14 +0300

    fix :)

--- RTCView.js
+++ RTCView.js
@@ -4,6 +4,7 @@ import {
   NativeModules,
   requireNativeComponent,
 } from 'react-native';
+import * as React from 'react';
 import PropTypes from 'prop-types';
 
 const {WebRTCModule} = NativeModules;
